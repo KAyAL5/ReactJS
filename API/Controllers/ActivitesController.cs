3b128af0a9d9f63e016be7535e2340439601b8bd
MSDN Chenoa <msdn@chenoainc.com>
2022-09-25 17:38:42 +0530

    my first commit

---
+++ API/Controllers/ActivitesController.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Presistence;
+using System.Linq;
+
+namespace API.Controllers
+{
+    public class ActivitesController : BaseAPIController
+    {
+        private readonly DataContext _context;
+        public ActivitesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetAllActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
---
+++ Presistence/Presistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Presistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions option) : base(option)
+        {
+
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
